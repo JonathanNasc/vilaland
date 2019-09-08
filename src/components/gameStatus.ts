ee61f4414deb34ed76e42d91cee15b9695727d0a
Jonathan Nascimento <jonathan.nasc@yahoo.com.br>
2019-09-08 15:06:40 -0300

    player value data

--- src/components/gameStatus.ts
+++ src/components/gameStatus.ts
@@ -1,42 +1,61 @@
 import "phaser"
 
 const TileSprite = Phaser.GameObjects.TileSprite;
+const inicialValue = 999;
 
 export class GameStatus {
 
-  public static init(scene: Phaser.Scene) {
-    if (scene.cameras.main.deadzone) {
-      return;
-    }
+  static gold: Phaser.GameObjects.TileSprite;
+  static stone: Phaser.GameObjects.TileSprite;
+  static bronze: Phaser.GameObjects.TileSprite;
+  static wood: Phaser.GameObjects.TileSprite;
 
-    GameStatus.initWood(scene);
+  public static init(scene: Phaser.Scene) {
+    GameStatus.initGold(scene);
     GameStatus.initStone(scene);
     GameStatus.initBronze(scene);
-    GameStatus.initGold(scene);
-  }
-
-  private static initWood(scene: Phaser.Scene) {
-    scene.add.rectangle(50, 50, 50, 40, 0x254638);
-    scene.add.rectangle(100, 50, 50, 40, 0x4c7564);
-    scene.add.existing(new TileSprite(scene, 50, 50, 47, 30, 'tileset', 'wood'));
-  }
-
-  private static initStone(scene: Phaser.Scene) {
-    scene.add.rectangle(50, 100, 50, 40, 0x254638);
-    scene.add.rectangle(100, 100, 50, 40, 0x4c7564);
-    scene.add.existing(new TileSprite(scene, 50, 100, 36, 32, 'tileset', 'stone_small_x'));
-  }
-
-  private static initBronze(scene: Phaser.Scene) {
-    scene.add.rectangle(50, 150, 50, 40, 0x254638);
-    scene.add.rectangle(100, 150, 50, 40, 0x4c7564);
-    scene.add.existing(new TileSprite(scene, 50, 150, 36, 32, 'tileset', 'bronze_stone_s'));
+    GameStatus.initWood(scene);
   }
 
   private static initGold(scene: Phaser.Scene) {
-    scene.add.rectangle(50, 200, 50, 40, 0x254638);
-    scene.add.rectangle(100, 200, 50, 40, 0x4c7564);
-    scene.add.existing(new TileSprite(scene, 50, 200, 36, 32, 'tileset', 'gold'));
+    this.createRectanglePair(scene, 40);
+    GameStatus.gold = this.createStatusIcon(scene, 40, 'gold');
+  }
+
+  private static initStone(scene: Phaser.Scene) {
+    this.createRectanglePair(scene, 80);
+    GameStatus.stone = this.createStatusIcon(scene, 80, 'stone_small_x');
+  }
+
+  private static initBronze(scene: Phaser.Scene) {
+    this.createRectanglePair(scene, 120);
+    GameStatus.bronze = this.createStatusIcon(scene, 120, 'bronze_stone_s', 35);
+  }
+
+  private static initWood(scene: Phaser.Scene) {
+    this.createRectanglePair(scene, 160);
+    GameStatus.wood = this.createStatusIcon(scene, 160, 'wood', 48, 30);
+  }
+
+  private static createRectanglePair(scene: Phaser.Scene, y: number) {
+    scene.add.rectangle(40, y, 35, 30, 0x3C6776, 0.5).setScrollFactor(0);
+    scene.add.rectangle(80, y, 45, 30, 0x2C5564, 0.5).setScrollFactor(0);
+  }
+
+  private static createStatusIcon(scene: Phaser.Scene, y: number, key: string, width = 36, height = 32): Phaser.GameObjects.TileSprite {
+    let tileSprite = new TileSprite(scene, 40, y, width, height, 'tileset', key);
+    let text = scene.add.text(65, y - 7, inicialValue.toString());
+    tileSprite.setScale(0.6);
+    tileSprite.setScrollFactor(0);
+    tileSprite.setDataEnabled();
+    tileSprite.setData('value', inicialValue);
+
+    tileSprite.on('setData', (gameObject: any, key: string, value: any) => {
+      if (key == 'value') text.setText(value + "");
+    });
+
+    scene.add.existing(tileSprite);
+    return tileSprite;
   }
 
 }
